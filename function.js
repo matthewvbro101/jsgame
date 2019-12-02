e4a89bd0d96fb2125d8a3fdfb5097a825b3b9b35
matthewvbro101 <masteryblackcraft@gmail.com>
2019-12-01 22:27:03 -0800

    init3

--- function.js
+++ function.js
@@ -1,9 +1,17 @@
 var player = document.getElementById('player');
 var box = document.getElementById('box');
+var ball = document.getElementById('ball');
 var playerWidth = player.offsetWidth;
 var playerHeight = player.offsetHeight;
 var boxWidth = box.offsetWidth;
 var boxHeight = box.offsetHeight;
+var ballWidth = ball.offsetWidth;
+var ballHeight = ball.offsetHeight;
+var speed = {
+    x: -5, 
+    y: 5
+};
+
 function moveLeft() {
     player.style.left = parseInt(player.offsetLeft) - 5 + 'px';
 }
@@ -11,7 +19,6 @@ function moveLeft() {
 function moveRight() {
     player.style.left = parseInt(player.offsetLeft) + 5 + 'px';
 }
-
 document.addEventListener('keydown', (e) => {
     switch (e.keyCode) {
         case 37:
@@ -29,6 +36,36 @@ document.addEventListener('keydown', (e) => {
             }
             // console.log("right key");
             break;
-
     }
 });
+
+function checkBounds() {
+    if (ball.offsetLeft < 0) {
+        ball.offsetLeft = 0;
+        speed.x *= -1;
+    }
+    if (ball.offsetTop < 0) {
+        ball.offsetTop = 0;
+        speed.y *= -1;
+    }
+}
+
+setInterval(function(){
+    ball.style.left = ball.offsetLeft + speed.x + 'px';
+    ball.style.top = ball.offsetTop + speed.y + 'px';
+
+    checkBounds();
+
+    if (ball.offsetTop > boxHeight-ball.offsetHeight-playerHeight) {
+        var ballRight = ball.offsetLeft + ballWidth;
+        var playerRight = player.offsetLeft + playerWidth;
+        if (ballRight > player.offsetLeft && ball.offsetLeft < playerRight) {
+            speed.y *= -1;
+            // console.log("inside");
+        }
+        // console.log("test");
+    }
+    if (ball.offsetTop > boxHeight-ball.offsetHeight){
+        box.removeChild(ball);
+    }
+}, 100);
