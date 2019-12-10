7b54e119e07980dd75ef15645eda471b7ea704f3
matthewvbro101 <masteryblackcraft@gmail.com>
2019-12-09 20:03:26 -0800

    init12

--- function.js
+++ function.js
@@ -1,10 +1,32 @@
 var player = document.getElementById('player');
 var box = document.getElementById('box');
-
-var playerWidth = player.offsetWidth;
-var playerHeight = player.offsetHeight;
 var boxWidth = box.offsetWidth;
 var boxHeight = box.offsetHeight;
+
+var ballColors = ['red', 'blue', 'orange', 'green'];
+var initSpeed = 1;
+
+class Score {
+    el;
+    value;
+
+    constructor(id) {
+        this.el = document.getElementById(id);
+        this.resetScore();
+    }
+    increaseScore() {
+        this.value++;
+        this.draw();    
+    }
+    resetScore() {
+        this.value = 0;
+        this.draw();
+    }
+    draw() {
+        this.el.innerHTML = 'Score: ' + this.value; 
+    }
+}
+var mainScore = new Score('score');
 
 class Ball {
     speed;
@@ -28,45 +50,68 @@ class Ball {
 
     checkBounds() {
         var ball = this.el;
+        var newX = ball.offsetLeft + this.speed.x;
+        var newY = ball.offsetTop + this.speed.y;
     
-        if (ball.offsetLeft < 0) {
-            ball.style.left = '0px';
+        if (newX < 0) {
+            newX = 0;
             this.speed.x *= -1;
+            this.increaseSpeed();
         }
-        if (ball.offsetTop < 0) {
-            ball.style.top = '0px';
+        if (newY < 0) {
+            newY = 0;
             this.speed.y *= -1;
+            this.increaseSpeed();
         }
-        if (ball.offsetLeft > (boxWidth-ball.offsetHeight)) {
-            ball.style.left = (boxWidth - ball.offsetHeight) + 'px';
+        if (newX > (boxWidth-ball.offsetHeight)) {
+            newX = (boxWidth - ball.offsetHeight);
             this.speed.x *= -1;
+            this.increaseSpeed();
         }
-    }
-
-    ballMovement() {
-        var ball = this.el;
     
-        ball.style.left = ball.offsetLeft + this.speed.x + 'px';
-        ball.style.top = ball.offsetTop + this.speed.y + 'px';
-        if (ball.offsetTop > boxHeight-ball.offsetHeight-playerHeight) {
-            var ballRight = ball.offsetLeft + ball.offsetWidth;
-            var playerRight = player.offsetLeft + playerWidth;
-            if (ballRight > player.offsetLeft && ball.offsetLeft < playerRight) {
+        if (newY > boxHeight-ball.offsetHeight) {
+            mainScore.resetScore();
+            box.removeChild(ball);
+            createBall();
+            return;
+        }
+
+        if (newY > boxHeight-ball.offsetHeight - player.offsetHeight) {
+            var ballRight = newX + ball.offsetWidth;
+            var playerRight = player.offsetLeft + player.offsetWidth;
+            if (ballRight > player.offsetLeft && newX < playerRight) {
                 this.speed.y *= -1;
+                mainScore.increaseScore();
+                this.increaseSpeed();
             }
         }
-        if (ball.offsetTop > boxHeight-ball.offsetHeight) {
-            box.removeChild(ball);
-        }
+
+        ball.style.left = newX + 'px';
+        ball.style.top = newY + 'px';
+    }
+    increaseSpeed() {
+        this.speed.x *= 1.1;
+        this.speed.y *= 1.1;
     }
 }
 
-var ballObjs = [
-    new Ball('ball', {x:5, y:-5}, 'blue', 10),
-    new Ball('ball2', {x:-5, y:-5}, 'green', 10),
-    new Ball('ball3', {x:-10, y:-5}, 'yellow', 15),
+var ballObjs = [    
+    // new Ball('ball', {x:5, y:-5}, 'blue', 10),
+    // new Ball('ball2', {x:-5, y:-5}, 'green', 10),
+    // new Ball('ball3', {x:-10, y:-5}, 'yellow', 15),
 ];
 
+function createBall() {
+    var color = ballColors[Math.floor(Math.random()*ballColors.length)];
+    var speed = {
+        x: (Math.floor(Math.random()*20)-10)*initSpeed,
+        y: -5*initSpeed
+    };
+    var newBall = new Ball('ball', speed, color, 15+Math.floor(Math.random()*11));
+    // initSpeed += 0.3;
+    ballObjs.push(newBall);
+}
+createBall();
 
 function moveLeft() {
     player.style.left = parseInt(player.offsetLeft) - 5 + 'px';
@@ -87,10 +132,11 @@ document.addEventListener('keydown', (e) => {
             break;
         case 39:
             moveRight();
-            if (player.offsetLeft > boxWidth-playerWidth) {
-                player.style.left = boxWidth-playerWidth + 'px';
+            if (player.offsetLeft > boxWidth - player.offsetWidth) {
+                player.style.left = boxWidth - player.offsetWidth + 'px';
             }
-            // console.log("right key");
+        case 32:
+            // createBall();
             break;
     }
 });
@@ -98,6 +144,5 @@ document.addEventListener('keydown', (e) => {
 setInterval(function(){
     for (var i=0; i<ballObjs.length; i++) {
         ballObjs[i].checkBounds();
-        ballObjs[i].ballMovement();
     }
 }, 100);
