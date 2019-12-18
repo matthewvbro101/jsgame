f5c682812f66f767f6f99e86aa46372eec765d56
matthewvbro101 <masteryblackcraft@gmail.com>
2019-12-17 18:17:39 -0800

    added more and changes

--- function.js
+++ function.js
@@ -47,7 +47,7 @@ class Ball {
         this.el.style.width = this.size + 'px';
         this.el.style.height = this.size + 'px';
     }
-
+    
     checkBounds() {
         var ball = this.el;
         var newX = ball.offsetLeft + this.speed.x;
@@ -87,12 +87,28 @@ class Ball {
             return;
         }
 
+        for (var i=0; i<bricks.length; i++) {
+            var brick = bricks[i];
+            this.processHit(newX, newY, brick);
+        }
         ball.style.left = newX + 'px';
         ball.style.top = newY + 'px';
     }
     increaseSpeed() {
         this.speed.x *= 1.1;
         this.speed.y *= 1.1;
+    }
+    processHit(x, y, brick) {
+        var brickEl = brick.el;
+        if (x < brickEl.offsetLeft + brickEl.offsetWidth && (x + this.size) > brickEl.offsetLeft &&
+            y < brickEl.offsetTop + brickEl.offsetHeight && (y + this.size) > brickEl.offsetTop) {
+            if (x > brickEl.offsetLeft && (x + this.size) < brickEl.offsetLeft + brickEl.offsetWidth) {
+                this.speed.y *= -1;
+            }
+            if (y > brickEl.offsetTop && (y + this.size) < brickEl.offsetTop + brickEl.offsetHeight) {
+                this.speed.x *= -1;
+            }
+        }
     }
 }
 
@@ -102,11 +118,36 @@ var ballObjs = [
     // new Ball('ball3', {x:-10, y:-5}, 'yellow', 15),
 ];
 
+class Bricks {
+    el;
+    color;
+    size;
+    
+    constructor(id, color, size) {
+        this.el = document.createElement(id);
+        this.el.classList.add("brick");
+        box.appendChild(this.el);
+
+        this.color = color;
+        this.size = size; 
+        this.el.style.backgroundColor = this.color;
+        this.el.style.left = Math.floor(Math.random()*(boxWidth-this.el.offsetWidth)) + 'px';
+        this.el.style.top = Math.floor(Math.random()*(boxHeight-this.el.offsetHeight)) + 'px';
+    }
+}
+var bricks = [];
+
+createBrick();
+function createBrick() {
+    var newBrick = new Bricks('bricks', 'green', 10);
+    bricks.push(newBrick);
+}
+
 function createBall() {
     var color = ballColors[Math.floor(Math.random()*ballColors.length)];
     var speed = {
         x: (Math.floor(Math.random()*20)-10)*initSpeed,
-        y: -5*initSpeed
+        y: -10*initSpeed
     };
     var newBall = new Ball('ball', speed, color, 15+Math.floor(Math.random()*11));
     // initSpeed += 0.3;
